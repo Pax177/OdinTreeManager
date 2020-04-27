66263abc81e2ea38939aba388001ad312acd37f9
VasilchukVV <VasilchukVV@gmail.com>
2020-04-27 11:30:55 +0300

    Unity Initial commit

---
+++ OdinTreeManager/Assets/Script/Editor/TreeManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Containerization.Tests
+{
+    [TestFixture]
+    public sealed class TreeManagerTest
+    {
+        [TestCase]
+        public async Task CreationTest()
+        {
+            var userContext = UserContextFactory.CreateUserContext();
+            var treeManager = userContext.TreeManagerFactory.CreateTreeManager();
+            var tree = await treeManager.CreateTree("123");
+            ShowFolder(tree);
+        }
+
+        private void ShowFolder(IFolder folder)
+        {
+            Console.WriteLine($"folder Id:{folder.Id} name{folder.Name}");
+
+            foreach (var subfolder in folder.Childs)
+            {
+                ShowFolder(subfolder);
+            }
+
+            foreach (var data in folder.Data)
+            {
+                ShowData(data);
+            }
+        }
+
+        private void ShowData(IData data)
+        {
+            Console.WriteLine($"data Id:{data.Id} Value:{data.Value()} type:{data.Type.Name}");
+        }
+    }
+}
